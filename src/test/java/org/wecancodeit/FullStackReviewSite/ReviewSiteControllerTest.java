e5aaad2e64167f151a9ffd58583c1b3769e39cec
KevinCobb <Bender953@Gmail.com>
2018-06-30 16:13:39 -0400

    update

--- src/test/java/org/wecancodeit/FullStackReviewSite/ReviewSiteControllerTest.java
+++ src/test/java/org/wecancodeit/FullStackReviewSite/ReviewSiteControllerTest.java
@@ -1,30 +1,27 @@
 package org.wecancodeit.FullStackReviewSite;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.*;
-	import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-	import javax.annotation.Resource;
-	import org.mockito.Mock;
-	import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-	import org.springframework.boot.test.mock.mockito.MockBean;
-	import org.springframework.test.web.servlet.MockMvc;
-	import org.wecancodeit.FullStackReviewSite.Review;
-	import org.wecancodeit.FullStackReviewSite.ReviewSiteController;
-	import org.wecancodeit.FullStackReviewSite.ReviewRepository;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReviewSiteController.class)
 
 	public class ReviewSiteControllerTest {
-	@RunWith(SpringRunner.class)
-	@WebMvcTest(ReviewSiteController.class)
-
-	public class ReviewsSiteControllerTest {
+	
 
 			@Resource
 			MockMvc mvc;
@@ -56,18 +53,18 @@ import static org.hamcrest.Matchers.*;
 			}
 			@Test
 				public void geteviewShouldReturnReviewView() throws Exception {
-					mvc.perform(get("/reviews/42")).andExpect(view().name(is(equalTo("review"))));
+					mvc.perform(get("/reviews/")).andExpect(view().name(is(equalTo("review"))));
 				}
 			@Test
 				public void getReviewShouldBeOk() throws Exception {
-					mvc.perform(get("/reviews/42")).andExpect(status().is2xxSuccessful());
+					mvc.perform(get("/reviews/")).andExpect(status().is2xxSuccessful());
 				}
 			@Test
 				public void getReviewShouldReturnOneReview() throws Exception {
-					mvc.perform(get("/reviews/2")).andExpect(model().attribute("review", is(repo.findOne(2L))));
+					mvc.perform(get("/reviews/")).andExpect(model().attribute("review", is(repo.findOne(2L))));
 				}
 			
 		}
 		
 		
-	}
+	
